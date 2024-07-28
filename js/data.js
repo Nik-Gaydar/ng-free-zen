b019473d8ab0889870da2c61bd534a20a711c1af
Nik_Laptop <NikGaydar@yandex.ru>
2024-07-28 22:18:38 +0700

    WIP

---
+++ js/data.js
@@ -0,0 +1,101 @@
+const download = {
+  title: 'Peaceful shopping mindful money',
+  texts: ['Paragraph 1'],
+  links: [
+    {
+      name: 'apple',
+      url: 'https://apple.com',
+    },
+    {
+      name: 'google',
+      url: 'https://google.com',
+    },
+  ],
+  image: {
+    source: './assets/images/1.jpg',
+    description: 'Bag and device',
+  },
+};
+
+console.log(download);
+
+const warranty = {
+  title: '1-year warranty boost',
+  texts: ['Paragraph 1', 'Paragraph 2'],
+  image: {
+    source: './assets/images/2.jpg',
+    description: 'Basket',
+  },
+};
+
+console.log(warranty);
+
+const care = {
+  title: 'ZEN Care',
+  texts: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3'],
+  image: {
+    source: './assets/images/3.jpg',
+    description: 'Card',
+  },
+};
+
+console.log(care);
+
+const cashback = {
+  title: 'and cashback!',
+  texts: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3'],
+  buttonText: 'Order a consultation',
+};
+
+console.log(cashback );
+
+// const clients = {
+//   brand_1: [
+//     {
+//       source: 'assets/brands/1.svg',
+//       description: 'Brand 1',
+//     },
+//   ],
+//   brand_2: [
+//     {
+//       source: 'assets/brands/2.svg',
+//       description: 'Brand 2',
+//     },
+//   ],
+//   brand_3: [
+//     {
+//       source: 'assets/brands/3.svg',
+//       description: 'Brand 3',
+//     },
+//   ],
+//   brand_4: [
+//     {
+//       source: 'assets/brands/4.svg',
+//       description: 'Brand 4',
+//     },
+//   ],
+//   brand_5: [
+//     {
+//       source: 'assets/brands/5.svg',
+//       description: 'Brand 5',
+//     },
+//   ],
+//   brand_6: [
+//     {
+//       source: 'assets/brands/6.svg',
+//       description: 'Brand 6',
+//     },
+//   ],
+//   brand_7: [
+//     {
+//       source: 'assets/brands/7.svg',
+//       description: 'Brand 7',
+//     },
+//   ],
+//   brand_8: [
+//     {
+//       source: 'assets/brands/8.svg',
+//       description: 'Brand 8',
+//     },
+//   ],
+// }
--- js/ui/warranty.js
+++
@@ -1,5 +0,0 @@
-const warranty {
-  title: '1-year warranty boost',
-  paragraph_1: 'ZEN cards have a very neat feature that will help you whenever your electronic devices start failing after the standard warranty ends, which is pretty common.',
-  
-}
